e1d552267c239f1496c6c63aeadc5d820c15327f
Friedrich Lindenberg <friedrich@pudo.org>
2019-02-02 21:48:53 +0100

    Bump version: 0.0.1 → 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='entityextractor',
-    version='0.0.1',
+    version='1.0.0',
     license='MIT',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
